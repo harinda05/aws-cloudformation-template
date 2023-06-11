1771ecb2c9cee5d20ca0cdeb2c6d6a1f2455cd1d
harinda05 <harindaburndown@gmail.com>
2023-06-11 08:02:46 -0700

    import helloworld event json from external module

--- sam-base-application/lambda/tests/unit/hello-world-handler.test.ts
+++ sam-base-application/lambda/tests/unit/hello-world-handler.test.ts
@@ -2,63 +2,17 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { handler } from '../../hello-world';
 import { expect, describe, it } from '@jest/globals';
 
-describe('Unit test for hello-world handler', function () {
-    it('verifies successful response', async () => {
-        const event: APIGatewayProxyEvent = {
-            httpMethod: 'get',
-            body: '',
-            headers: {},
-            isBase64Encoded: false,
-            multiValueHeaders: {},
-            multiValueQueryStringParameters: {},
-            path: '/hello',
-            pathParameters: {},
-            queryStringParameters: {},
-            requestContext: {
-                accountId: '123456789012',
-                apiId: '1234',
-                authorizer: {},
-                httpMethod: 'get',
-                identity: {
-                    accessKey: '',
-                    accountId: '',
-                    apiKey: '',
-                    apiKeyId: '',
-                    caller: '',
-                    clientCert: {
-                        clientCertPem: '',
-                        issuerDN: '',
-                        serialNumber: '',
-                        subjectDN: '',
-                        validity: { notAfter: '', notBefore: '' },
-                    },
-                    cognitoAuthenticationProvider: '',
-                    cognitoAuthenticationType: '',
-                    cognitoIdentityId: '',
-                    cognitoIdentityPoolId: '',
-                    principalOrgId: '',
-                    sourceIp: '',
-                    user: '',
-                    userAgent: '',
-                    userArn: '',
-                },
-                path: '/hello',
-                protocol: 'HTTP/1.1',
-                requestId: 'c6af9ac6-7b61-11e6-9a41-93e8deadbeef',
-                requestTimeEpoch: 1428582896000,
-                resourceId: '123456',
-                resourcePath: '/hello',
-                stage: 'dev',
-            },
-            resource: '',
-            stageVariables: {},
-        };
+import * as helloWorldEvent from '../../../events/hello-world-event.json';
+
+describe('Unit tests for lambda functions', function () {
+    it('helloWorldFunction verifies successful response', async () => {
+        const event: APIGatewayProxyEvent = helloWorldEvent;
         const result: APIGatewayProxyResult = await handler(event);
 
         expect(result.statusCode).toEqual(200);
         expect(result.body).toEqual(
             JSON.stringify({
-                message: 'hello world',
+                message: 'hello Harinda',
             }),
         );
     });
